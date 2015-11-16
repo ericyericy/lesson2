d50ae2bd92474fd58f3b7fcea403f8ba084fdff4
yuyueugene84 <yuyueugene84@gmail.com>
2015-11-17 00:42:57 +0800

    added homework1

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,33 @@
+# 先要使用者輸入剪刀石頭或布的其中一個選項
+# 接下來讓電腦亂數選出一個選項
+# 比較兩個選項，決定誰贏誰輸
+# 輸出結果
+# 問使用者是否再玩一次
+
+begin #可以美化輸出
+  puts "|===============================================|" 
+  puts "|Welcome to Rock Paper Scissors!!!              |"
+  puts "|===============================================|"
+
+  begin 
+    puts "please choose one of the following: R / P / S"
+    user_input = gets.chomp.upcase
+  end while !["R", "P", "S"].include?(user_input) 
+
+  #接下來請把剩下的部份寫出來...
+
+
+
+
+
+
+  #問使用者是否還要再玩 
+  begin
+    puts "Play Again?: Y / N"
+    continue = gets.chomp.upcase
+  end while !["Y", "N"].include?(continue)
+
+end whlie continue == "N"
+# 若使用者回答 "N"，印出離開的訊息，不是就回到迴圈的上層繼續玩
+puts "Good Bye! Thanks for playing!"
+
